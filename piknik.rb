bca03a03802abc445f84d3ebc3347fe2e258cb4d
Frank Denis <github@pureftpd.org>
2016-07-23 19:14:17 +0200

    Add a Homebrew formula for Piknik

---
+++ piknik.rb
@@ -0,0 +1,56 @@
+class Piknik < Formula
+  desc "Copy/paste anything over the network"
+  homepage "https://github.com/jedisct1/piknik"
+  url "https://github.com/jedisct1/piknik/archive/0.4.tar.gz"
+  sha256 "f453ec9e4948d84c0446a912812ee5de1fe0bb7213306061b015cad76cec9c18"
+  head "https://github.com/jedisct1/piknik.git"
+
+  depends_on "go" => :build
+  depends_on "godep" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    dir = buildpath/"src/github.com/jedisct1/"
+    dir.install Dir["*"]
+    ln_s buildpath/"src", dir
+    cd dir do
+      system "godep", "restore"
+      system "go", "build", "-o", bin/"piknik", "."
+      ln "zsh.aliases", "piknik.sh"
+      (prefix/"etc/profile.d").install "piknik.sh"
+    end
+  end
+
+  def caveats; <<-EOS.undent
+    For Bash or Zsh, put something like this in your $HOME/.bashrc or $HOME/.zshrc:
+      source $(brew --prefix)/etc/profile.d/piknik.sh
+    EOS
+  end
+
+  test do
+    begin
+      conffile = testpath/"testconfig.toml"
+
+      IO.popen([{}, "#{bin}/piknik", "-genkeys"]) do |genkeys|
+        lines = genkeys.readlines.grep(/\s+=\s+/).map { |x| x.gsub(/\s+/, " ").gsub(/#.*/, "") }.uniq
+        conffile.write lines.join("\n")
+      end
+      pid = fork do
+        exec "#{bin}/piknik", "-server", "-config", conffile
+      end
+      begin
+        IO.popen([{}, "#{bin}/piknik", "-config", conffile, "-copy"], "w+") do |p|
+          p.write "test"
+        end
+        IO.popen([{}, "#{bin}/piknik", "-config", conffile, "-move"], "r") do |p|
+          clipboard = p.read
+          assert_equal clipboard, "test"
+        end
+      ensure
+        Process.kill("TERM", pid)
+        Process.wait(pid)
+        conffile.unlink
+      end
+    end
+  end
+end
